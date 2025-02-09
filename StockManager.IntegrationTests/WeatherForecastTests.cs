17e2e0de88aecfb4ef54f3db13a43495cda982ab
carltonupperdine <carlton.upperdine@gmail.com>
2025-02-09 11:59:53 +0000

    Scaffolded API with basic integration test

---
+++ StockManager.IntegrationTests/WeatherForecastTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using StockManager.API;
+
+namespace StockManager.IntegrationTests;
+
+public class WeatherForecastTests(WebApplicationFactory<Program> webApplicationFactory)
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task GetRequestReturnsOk()
+    {
+        var client = webApplicationFactory.CreateClient();
+        var response = await client.GetAsync("/weatherforecast");
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+    }
+}
